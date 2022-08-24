a20ae5a8742ef87033215bb165e129e9b5f4840d
Antony12131 <99255954+Antony12131@users.noreply.github.com>
2022-08-24 19:34:15 +0500

    dotenv installed

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express();
+require('dotenv').config({path: './config/config.env'})
+// import routes
 const Bootcamps = require('./Routes/bootcamps')
 app.use('/bootcamps' ,Bootcamps)
-app.listen(5000, () => console.log("Server is running"));
+
+
+let PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
